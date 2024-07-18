80352e9e580b219b5ee67e4bee4e2b7570d9d95e
@Mikist <mikeochango3@gmail.com>
2024-07-18 14:33:44 +0300

    adedd entry labels and buttons and style them

--- main.py
+++ main.py
@@ -10,7 +10,23 @@ image = CTkImage(Image.open("resources/cover5.jpg"), size=(1300,778))
 imageLabel = CTkLabel(root,image=image, text="")
 imageLabel.place(x=0,y=0)
 
-headingLabel = CTkLabel(root,text="Employee Management System")
-headingLabel.place(x=20,y=100)
+headingLabel = CTkLabel(root,text="Employee Management System",bg_color="#3498eb",font=("Goudy Old Style",40,"bold"))
+headingLabel.place(x=550,y=10)
+
+usernameEntry = CTkEntry(root,placeholder_text="Enter your username", width=280,height=40,
+                        corner_radius=10, bg_color="#3498eb",
+                        font=("Arial", 14),)
+usernameEntry.place(x=20,y=260)
+
+passwordEntry = CTkEntry(root,placeholder_text="Enter your password", width=280,height=40,
+                        corner_radius=10, bg_color="#3498eb",
+                        font=("Arial", 14),show="*")
+passwordEntry.place(x=20,y=310)
+
+loginButton = CTkButton(root,text="Login",height=40,
+                        corner_radius=10,bg_color="#3498eb",
+                        font=("Arial", 14),fg_color="#4CAF50", 
+                        hover_color="#45A049",cursor = "hand2") 
+loginButton.place(x=80,y=360)
 
 root.mainloop()
