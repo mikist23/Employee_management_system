426e0f66162f61e3f8873077b5ef08d31e36306c
@Mikist <mikeochango3@gmail.com>
2024-07-18 13:17:35 +0300

    adedd background image and resources

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from customtkinter import *
+from PIL import Image
 
 root = CTk()
 root.resizable(0,0)
-root.geometry("1000x578")
+root.geometry("1300x778")
+root.title("Login page")
+
+image = CTkImage(Image.open("resources/cover5.jpg"), size=(1300,778))
+imageLabel = CTkLabel(root,image=image, text="")
+imageLabel.place(x=0,y=0)
+
+headingLabel = CTkLabel(root,text="Employee Management System")
+headingLabel.place(x=20,y=100)
+
 root.mainloop()
